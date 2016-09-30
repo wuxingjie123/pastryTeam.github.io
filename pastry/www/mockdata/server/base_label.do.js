04dd64c250836a271fb769af11bb7c866b4466ea
pastryTeam <176290392@qq.com>
2016-09-30 11:20:45 +0800

    添加 label 的模拟数据

---
+++ pastry/www/mockdata/server/base_label.do.js
@@ -0,0 +1,12 @@
+define({
+    "act=getText": [
+        {
+            delay: 1000,
+            data: {
+                TEXT: 'hello world',
+                Name: "Pastry Team",
+                PhoneNo: '17892237893'
+            }
+        }
+    ]
+});
