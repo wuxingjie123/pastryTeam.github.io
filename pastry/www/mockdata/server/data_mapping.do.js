30eb90616dbf8442316c0b638a8fd1a4a54eee08
pastryTeam <176290392@qq.com>
2016-10-03 21:28:03 +0800

    完善 mapping 示例代码

---
+++ pastry/www/mockdata/server/data_mapping.do.js
@@ -0,0 +1,13 @@
+define({
+    "act=getList2": [
+        {
+            delay: 1000,
+            data: {
+                mylist: [
+                    {id: '1', title: 500},
+                    {id: '2', title: 600}
+                ]
+            }
+        }
+    ]
+});
