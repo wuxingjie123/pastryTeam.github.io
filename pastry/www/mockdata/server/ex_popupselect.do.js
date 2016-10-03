c86c42c16b0e835eb3e78ae782f4a23d540a4c95
pastryTeam <176290392@qq.com>
2016-10-03 19:17:48 +0800

    完善 popup 系列 示例代码

---
+++ pastry/www/mockdata/server/ex_popupselect.do.js
@@ -0,0 +1,13 @@
+define({
+    "act=getPopupSelect": [
+        {
+            delay: 1000,
+            data: {
+                items: [
+                    {id: '1', title: 500},
+                    {id: '2', title: 600}
+                ]
+            }
+        }
+    ]
+});
